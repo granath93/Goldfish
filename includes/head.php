47151b4f9fea1f91c92cf3a0c1d52a25596470f9
granath93 <rysaren@hotmail.com>
2014-03-13 18:58:28 +0100

    Lägger upp lite grundfiler
    Lägger upp en del grundfiler som vi kommer kunna arbeta utifrån

---
+++ includes/head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width" charset="utf-8">
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<link rel="stylesheet" href="css/admin.css" >
+<script src="#" charset="utf-8"></script>
+	<title>Admin</title>
+</head>
+<body>
+
+<div class="content">
---
+++ includes/headFront.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width" charset="utf-8">
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<link rel="stylesheet" href="css/front.css" >
+<script src="#" charset="utf-8"></script>
+	<title>Front</title>
+</head>
+<body>
+
+<div class="content">
