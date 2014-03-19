f983576888b65334f6d71398aeb29ad421ed2aed
dlhagstroem <dennis.lh@live.se>
2014-03-19 11:33:47 +0100

    började på loginen

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+$pageTitle="Login";
+
+
+include("includes/db.php");
+
+?>
+
+<form>
+
+	User <input type="text" name="user"><br/>
+	Pass <input type="password" name="password"><br/>
+	<input type="submit"  />
+
+</form>
