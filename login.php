c7334a06d730dae0af07b28b255b345b953ff3a7
dlhagstroem <dennis.lh@live.se>
2014-03-19 13:36:48 +0100

    försöker så gott de går

--- login.php
+++ login.php
@@ -1,15 +1,54 @@
 <?php
 $pageTitle="Login";
 
-
 include("includes/db.php");
+if(!empty($_POST)){
+	$username = $_POST['user'];
+	$password = $_POST['password'];
+
+	try{
+		$result = $db->prepare("SELECT * from users where username = ? and password = ?");
+		$result->bindParam(1,$username);
+		$result->bindParam(2,$password);
+		$result->execute();
+	}catch(PDOException $e){
+		$e->getMessage();
+	}
+
+	$matches = $result->fetchRowCount();
+
+	if($matches){
+		$_SESSION['user_id'] = $matches->id;
+		header('location: loggedinpage.php');
+	}
+	else {
+		echo 'the oassword is wrong. please try again later.';
+	}
+
+	/*$sql = "SELECT * FROM Administrator where adminName = {$username} and adminPassword = {$password}";
+
+	$result = $mysqli->query($sql);
+	//print_r($result);
+	if($result->num_rows){
+		echo 'there is some admins in the database.';
+	}
+	else {
+		echo 'sorry, there is no user with that name/password';
+	}*/
+
+
+
+
+}
+
+
 
 ?>
 
-<form>
+<form method="post" action="login.php">
 
 	User <input type="text" name="user"><br/>
 	Pass <input type="password" name="password"><br/>
-	<input type="submit"  />
+	<input type="submit" name="submit" value="log in"  />
 
 </form>
