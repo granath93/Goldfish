6a44c074610833b4a6bf674147b3e3f44826b45b
dlhagstroem <dennis.lh@live.se>
2014-03-21 13:39:52 +0100

    Håller på med login, sliter

--- login.php
+++ login.php
@@ -1,40 +1,42 @@
 <?php
 $pageTitle="Login";
 
-include("includes/db.php");
+//error_reporting(0);
+require 'includes/db.php';
 
 
 	//med mysqli
-	$sql = "SELECT * FROM Administrator where adminName = {$username} and adminPassword = {$password}";
+if($result = $mysqli->query("SELECT * FROM Administrator"))
+{
+	if($count = $result->num_rows)
+	{
+		echo '<p>', $count, '</p>';
+		
+		while($row = $result->fetch_object());
+		{
+			echo $row->adminName,' ', $row->adminPassword, '<br>';
+		}
 
-	$result = $mysqli->query($sql);
-	//print_r($result);
-	if($result->num_rows){
-		echo 'there is some admins in the database.';
+		$result->free();
 	}
-	else {
-		echo 'sorry, there is no user with that name/password';
-	}
-
-
-
-
 }
+
+
+
+
+
 
 
 
 ?>
 
-<form method="post" action="login.php">
 
-	User <input type="text" name="user"><br/>
-	Pass <input type="password" name="password"><br/>
-	<input type="submit" name="submit" value="log in"  />
 
-</form>
 
-//med PDO
-/*
+<?php
+
+/* med PDO
+
 if(!empty($_POST)){
 	$username = $_POST['user'];
 	$password = $_POST['password'];
@@ -56,4 +58,12 @@ if(!empty($_POST)){
 	}
 	else {
 		echo 'the oassword is wrong. please try again later.';
+//formet
+		<form method="post" action="login.php">
+
+	User <input type="text" name="user"><br/>
+	Pass <input type="password" name="password"><br/>
+	<input type="submit" name="submit" value="log in"  />
+
+</form>
 	}*/
