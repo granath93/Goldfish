0c46c5c922991a01900abb6c4509f8b5cfe57144
granath93 <rysaren@hotmail.com>
2014-03-23 13:35:01 +0100

    Uppdaterat statistiksidan med CSS

--- indexAdmin.php
+++ indexAdmin.php
@@ -1,6 +1,17 @@
 <?php 
 $currentPage = "index";
 include("includes/headAdmin.php"); 
+include("includes/db.php");
+
+
+$res = $mysqli->query('SELECT * FROM Entry') or die("Could not query database" . $mysqli->errno . 
+" : " . $mysqli->error);
+
+while($row = $res->fetch_object()) { 
+$date = strtotime($row->timeStamp); 
+$entryId = ($row->entryId);
+
+}
 
 ?>
 
@@ -11,8 +22,55 @@ include("includes/headAdmin.php");
 <div class="content">
 
 <h1>Statistik</h1>
-	Här kommer massa statistik!
+
+<div class="statisticContent">
+<table>
+<tr>
+	<td>
+		<h3> Idag </h3> 
+	</td>
+	<td>
+		<h3> Denna vecka </h3>
+	</td>
+</tr>
+<tr>
+	<td>
+		<p> <?php echo count($entryId); ?> </p> 
+	</td>
+	<td>
+		<p><?php echo count($entryId); ?> </p> 
+	</td>
+</tr>
+<tr>
+	<td>
+		<h3> Denna månad </h3> 
+	</td>
+	<td>
+		<h3> Totalt </h3>
+	</td>
+</tr>
+<tr>
+	<td>
+		<p> <?php echo count($entryId); ?> </p> 
+	</td>
+	<td>
+		<p><?php echo count($entryId); ?></p> 
+	</td>
+</tr>
+
+</table>
 
 
 </div>
 <?php include("includes/footerAdmin.php"); ?>
+
+<!--
+
+entryId
+entryName
+entryImage
+entryColor
+timeStamp
+accepted
+designerId
+productId
