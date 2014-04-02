0c48b917d2745b7d9cb5d68b4c2bea0f69f940ca
dlhagstroem <dennis.lh@live.se>
2014-04-02 16:00:43 +0200

    försöker med bidragssida

--- entryAdmin.php
+++ entryAdmin.php
@@ -7,14 +7,24 @@ include("includes/db.php");
 
 
 
+
+
 ?>
 <div class="leftNav"></div>
 	<div class="content">
 		<img class="imgStyle" src="images/ggg.png" /> <br/>
+		<div class="entryText">
 			<p>BidragsNamn</p> <br/>
 			<p>Skapare</p> <br/>
 			<p>Ort</p> <br/>
-			<p>Röster</p> <br/>
-</div>
+			<p>Röster</p> 
+		</div>
+		<div class="testDiv">
+		<img src="images/godkannBtn.png" /> <br/>
+		<img src="images/tabortBtn.png" />
+		</div>
+	</div>
+
+
 
 <?php include("includes/footerAdmin.php"); ?>
