7c53f8e23683b9ebc057ff047f6424c6c1dc9fc3
granath93 <rysaren@hotmail.com>
2014-04-18 12:22:57 +0200

    lite bilder

--- upload_background.php
+++
@@ -1,9 +0,0 @@
-
-<!DOCTYPE html>
-<html>
-  <head></head>
-  <body>
-    <img src="images/logotype/logotype.jpg"/>
-
-  </body>
-</html>
--- upload_image.php
+++ upload_image.php
@@ -1,14 +1,14 @@
-
 <?php
 
  // I'm skipping a lloooot of verification steps on the file here.
  // Also, I'm assuming you uploaded a jpg.
 
- 	move_uploaded_file($_FILES['image']['tmp_name'], 'images/product/mask.jpg');
-
-
+ move_uploaded_file($_FILES['image']['tmp_name'], 'images/pic.jpg');
 ?>
-
-    <img style="width: 100%;" src="images/product/mask.jpg?v=<?php echo rand(0,1000) // rand() prevents the browser from displaying a previously cached image ?>"/>
- 
-<!--
+<!DOCTYPE html>
+<html>
+  <head></head>
+  <body>
+    <img style="width: 100%;" src="images/pic.jpg?v=<?php echo rand(0,1000) // rand() prevents the browser from displaying a previously cached image ?>"/>
+  </body>
+</html>
