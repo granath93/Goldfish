e4dc44c2fe47e6e3ef68a9bd943928f2665cb5ae
granath93 <rysaren@hotmail.com>
2014-04-23 17:53:13 +0200

    försökt lösa massa problem men det går inte!

--- includes/headFront.php
+++ includes/headFront.php
@@ -1,14 +1,56 @@
+<?php
+include("includes/db.php");
+
+$res = $mysqli->query('SELECT * FROM Appearance') or die("Could not query database" . $mysqli->errno . 
+" : " . $mysqli->error);
+
+	while($row = $res->fetch_object()) { 
+	$background = ($row->background);
+	
+	}
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta name="viewport" content="width=device-width" charset="utf-8">
-<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <link rel="stylesheet" href="css/normalize.css" >
 <link rel="stylesheet" href="css/front.css" >
-<script src="#" charset="utf-8"></script>
+<script src="js/functions.js" charset="utf-8"></script>
 	<title>Front</title>
 </head>
-<body>
+<body style="background-color:#<?php echo $background ?>">
 
-<div class="content">
+
+
+<!--	
+
+<div class="nav-container">
+<div class="nav">
+		
+			<a class="" href="#contentStart"> Start </a>
+			<a class="" href="index.php"> Designa </a>
+			<a class="" href="index.php"> Topplista </a>
+			<a class="" href="index.php"> Senaste Bidrag </a>
+			<a class="" href="#contentRules"> Regler </a>
+		
+	</div>
+</div>
+	
+
+
+-->
+
+	
+	<div class="topNav">
+		<nav>
+			<a class="" href="index.php"> Start </a>
+			<a class="" href="#contentScen"> Designa </a>
+			<a class="" href="#contentTopEntry"> Topplista </a>
+			<a class="" href="#contentLatestEntry"> Senaste Bidrag </a>
+			<a class="" href="#contentRules"> Regler </a>
+		</nav>
+	</div>
